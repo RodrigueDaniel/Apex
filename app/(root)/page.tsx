1fcd4fef211baad31c09c57e8b2f32f29a9d23ef
RodrigueDaniel <149811433+RodrigueDaniel@users.noreply.github.com>
2025-12-06 17:14:18 +0530

    fix: css

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -64,7 +64,7 @@ export default async function Dashboard() {
                                 ))}
                             </div>
                         ) : (
-                            <div className="flex flex-col items-center justify-center py-20 border border border-gray-800 rounded-2xl bg-white/5">
+                            <div className="flex flex-col items-center justify-center py-20 border border-gray-800 rounded-2xl bg-white/5">
                                 <div className="mb-6">
                                      <Image src="/assets/images/stock.png" alt="Empty" width={64} height={64} />
                                 </div>
